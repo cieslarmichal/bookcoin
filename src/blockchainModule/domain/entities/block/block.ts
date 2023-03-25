d0a652f6f886d506d1ba61acda48e6dbcbbdd70d
Michal Cieslar <michalovskyyy@gmail.com>
2023-03-25 13:02:22 +0100

    install needed npm packages

--- src/blockchainModule/domain/entities/block/block.ts
+++ src/blockchainModule/domain/entities/block/block.ts
@@ -4,7 +4,6 @@ import { Validator } from '../../../../libs/validator/validator.js';
 
 export const blockInputSchema = Schema.object({
   index: Schema.number(),
-  hash: Schema.string(),
   previousHash: Schema.string(),
   timestamp: Schema.number(),
   data: Schema.string(),
@@ -14,18 +13,22 @@ export type BlockInput = SchemaType<typeof blockInputSchema>;
 
 export class Block {
   public index: number;
-  public hash: string;
   public previousHash: string;
   public timestamp: number;
   public data: string;
 
   public constructor(input: BlockInput) {
-    const { index, hash, previousHash, timestamp, data } = Validator.validate(blockInputSchema, input);
+    const { index, previousHash, timestamp, data } = Validator.validate(blockInputSchema, input);
 
     this.index = index;
-    this.hash = hash;
     this.previousHash = previousHash;
     this.timestamp = timestamp;
     this.data = data;
+
+    this.hash = hash;
+  }
+
+  private calculateHash(): string {
+    CryptoJS.SHA256(String(this.index) + this.previousHash + String(this.timestamp) + this.data).toString();
   }
 }
