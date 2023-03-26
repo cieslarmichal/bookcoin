2244abdc4a8eeafdd737ed5f252a815ed08a066d
Michal Cieslar <michalovskyyy@gmail.com>
2023-03-26 09:36:48 +0200

    add main

--- src/envKey.ts
+++ src/envKey.ts
@@ -2,4 +2,5 @@ export enum EnvKey {
   logLevel = 'LOG_LEVEL',
   httpHost = 'HTTP_HOST',
   httpPort = 'HTTP_PORT',
+  peerToPeerPort = 'PEER_TO_PEER_PORT',
 }
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,48 @@
-import 'reflect-metadata';
-
 import dotenv from 'dotenv';
+import express, { json } from 'express';
 
-import { EnvKey } from './envKey';
+import { EnvKey } from './envKey.js';
+import { LogLevel } from './libs/logger/logLevel.js';
 
 async function main(): Promise<void> {
   dotenv.config();
 
-  const logLevel = process.env[EnvKey.logLevel];
+  const logLevel = process.env[EnvKey.logLevel] as LogLevel;
   const httpHost = process.env[EnvKey.httpHost];
   const httpPort = Number(process.env[EnvKey.httpPort]);
+  const peerToPeerPort = Number(process.env[EnvKey.peerToPeerPort]);
 
   console.log({
     logLevel,
     httpHost,
     httpPort,
+    peerToPeerPort,
+  });
+
+  const app = express();
+
+  app.use(json());
+
+  app.get('/blocks', (req, res) => {
+    res.send(getBlockchain());
+  });
+
+  app.post('/mineBlock', (req, res) => {
+    const newBlock: Block = generateNextBlock(req.body.data);
+    res.send(newBlock);
+  });
+
+  app.get('/peers', (req, res) => {
+    res.send(getSockets().map((s: any) => s._socket.remoteAddress + ':' + s._socket.remotePort));
+  });
+
+  app.post('/addPeer', (req, res) => {
+    connectToPeers(req.body.peer);
+    res.send();
+  });
+
+  app.listen(httpPort, httpHost, () => {
+    console.log('Listening http on port: ' + myHttpPort);
   });
 }
 
