26627a0ad1ba5ebf507b48f65e499f2ae1da0481
Michal Cieslar <michalovskyyy@gmail.com>
2023-04-01 11:41:12 +0200

    add domain events class

--- src/common/domain/events/domainEvents.ts
+++ src/common/domain/events/domainEvents.ts
@@ -1,76 +1,70 @@
+import { DomainEvent } from './domainEvent';
+import { AggregateRoot } from '../aggregateRoot';
+import { UniqueId } from '../uniqueId';
+
+type RegisterEventCallback = (event: DomainEvent) => Promise<void>;
+
 export class DomainEvents {
-  private static handlersMap = {};
-  private static markedAggregates: AggregateRoot<any>[] = [];
+  private static eventToHandlersMapping: Record<string, RegisterEventCallback[]> = {};
+  private static markedAggregates: AggregateRoot<unknown>[] = [];
 
-  /**
-   * @method markAggregateForDispatch
-   * @static
-   * @desc Called by aggregate root objects that have created domain
-   * events to eventually be dispatched when the infrastructure commits
-   * the unit of work.
-   */
+  public static register(callback: RegisterEventCallback, eventName: string): void {
+    if (!this.eventToHandlersMapping[eventName]) {
+      this.eventToHandlersMapping[eventName] = [];
+    }
 
-  public static markAggregateForDispatch(aggregate: AggregateRoot<any>): void {
-    const aggregateFound = !!this.findMarkedAggregateByID(aggregate.id);
+    (this.eventToHandlersMapping[eventName] as RegisterEventCallback[]).push(callback);
+  }
 
-    if (!aggregateFound) {
+  public static markAggregateForDispatch(aggregate: AggregateRoot<unknown>): void {
+    const foundAggregate = this.markedAggregates.find((aggregate) => aggregate.id.equals(aggregate.id));
+
+    if (!foundAggregate) {
       this.markedAggregates.push(aggregate);
     }
   }
 
-  private static dispatchAggregateEvents(aggregate: AggregateRoot<any>): void {
-    aggregate.domainEvents.forEach((event: IDomainEvent) => this.dispatch(event));
-  }
-
-  private static removeAggregateFromMarkedDispatchList(aggregate: AggregateRoot<any>): void {
-    const index = this.markedAggregates.findIndex((a) => a.equals(aggregate));
-    this.markedAggregates.splice(index, 1);
-  }
-
-  private static findMarkedAggregateByID(id: UniqueEntityID): AggregateRoot<any> {
-    let found: AggregateRoot<any> = null;
-    for (const aggregate of this.markedAggregates) {
-      if (aggregate.id.equals(id)) {
-        found = aggregate;
-      }
-    }
-
-    return found;
-  }
-
-  public static dispatchEventsForAggregate(id: UniqueEntityID): void {
-    const aggregate = this.findMarkedAggregateByID(id);
-
-    if (aggregate) {
-      this.dispatchAggregateEvents(aggregate);
-      aggregate.clearEvents();
-      this.removeAggregateFromMarkedDispatchList(aggregate);
-    }
-  }
-
-  public static register(callback: (event: IDomainEvent) => void, eventClassName: string): void {
-    if (!this.handlersMap.hasOwnProperty(eventClassName)) {
-      this.handlersMap[eventClassName] = [];
-    }
-    this.handlersMap[eventClassName].push(callback);
-  }
-
   public static clearHandlers(): void {
-    this.handlersMap = {};
+    this.eventToHandlersMapping = {};
   }
 
   public static clearMarkedAggregates(): void {
     this.markedAggregates = [];
   }
 
-  private static dispatch(event: IDomainEvent): void {
-    const eventClassName: string = event.constructor.name;
+  public static async dispatchEventsForAggregate(id: UniqueId): Promise<void> {
+    const foundAggregate = this.markedAggregates.find((aggregate) => aggregate.id.equals(id));
 
-    if (this.handlersMap.hasOwnProperty(eventClassName)) {
-      const handlers: any[] = this.handlersMap[eventClassName];
-      for (const handler of handlers) {
-        handler(event);
-      }
+    if (foundAggregate) {
+      this.dispatchAggregateEvents(foundAggregate);
+
+      foundAggregate.clearEvents();
+
+      this.removeAggregateFromMarkedAggregates(foundAggregate);
     }
+  }
+
+  private static async dispatchAggregateEvents(aggregate: AggregateRoot<unknown>): Promise<void> {
+    await Promise.all(
+      aggregate.events.map(async (event) => {
+        const eventHandlers = this.eventToHandlersMapping[event.name];
+
+        if (!eventHandlers) {
+          return;
+        }
+
+        await Promise.all(
+          eventHandlers.map(async (eventHandler) => {
+            await eventHandler(event);
+          }),
+        );
+      }),
+    );
+  }
+
+  private static removeAggregateFromMarkedAggregates(aggregate: AggregateRoot<unknown>): void {
+    const foundIndex = this.markedAggregates.findIndex((markedAggregate) => markedAggregate.equals(aggregate));
+
+    this.markedAggregates.splice(foundIndex, 1);
   }
 }
